ae915756b04665a774856481c918069c79e4d42d
Silandrin <xpoh@inbox.ru>
2017-03-02 10:06:04 +0300

    Восьмое задание
    Коммит для 8го задания. Проверка количества стикеров у товаров.

---
+++ javatests/src/test/java/ru/stqa/training/selenium/MainPage.java
@@ -0,0 +1,43 @@
+package ru.stqa.training.selenium;
+
+import org.testng.annotations.*;
+import org.testng.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by aataushk on 02.03.2017.
+ */
+public class MainPage {
+
+    private WebDriver driver;
+
+    @BeforeClass
+    public void start() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void duckStickerCheck() {
+        driver.get("http://localhost/litecart/en/");
+
+        //Убедимся, что у каждой утки есть МИНИМУМ один стикер
+        for (WebElement image : driver.findElements(By.cssSelector("div.image-wrapper"))) {
+            image.findElement(By.cssSelector("div.sticker"));
+        }
+
+        //Убедимся, что стикеров столько же, сколько и уток, а значит у каждой лишь по одному
+        Assert.assertEquals(driver.findElements(By.cssSelector("div.sticker")).size(),driver.findElements(By.cssSelector("div.image-wrapper")).size(), "Стикеров больше, чем уток!");
+
+    }
+
+    @AfterClass
+    public void stop(){
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+}
